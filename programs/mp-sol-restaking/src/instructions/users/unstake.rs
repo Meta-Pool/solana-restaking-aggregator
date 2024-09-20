d2b6cb2db4df6b8130d1c152f6148dd057a7dda2
Lucio Tato <luciotato@gmail.com>
2024-09-21 02:07:56 +0900

    add withdrawal fee mpsol to unstake

--- programs/mp-sol-restaking/src/instructions/users/unstake.rs
+++ programs/mp-sol-restaking/src/instructions/users/unstake.rs
@@ -1,8 +1,8 @@
 use crate::{constants::*, error::ErrorCode, MainVaultState, UnstakeTicket};
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::pubkey::Pubkey;
-use anchor_spl::token::{burn, Burn, Mint, Token, TokenAccount};
-use shared_lib::mpsol_amount_to_sol_value;
+use anchor_spl::token::{burn, Burn, Mint, Token, TokenAccount, Transfer};
+use shared_lib::{apply_bp, mpsol_amount_to_sol_value};
 
 #[derive(Accounts)]
 /// Unstake: burn mpSOL and get an unstake-ticket for the SOL-value of the mpSOL burned
@@ -19,6 +19,8 @@ pub struct Unstake<'info> {
 
     #[account(mut)]
     pub mpsol_mint: Box<Account<'info, Mint>>,
+    #[account(mut, token::mint = mpsol_mint)]
+    pub treasury_mpsol_account: Account<'info, TokenAccount>,
 
     #[account(init, payer = unstaker, space = 8 + UnstakeTicket::INIT_SPACE)]
     pub new_ticket_account: Account<'info, UnstakeTicket>,
@@ -28,9 +30,23 @@ pub struct Unstake<'info> {
 }
 
 pub fn handle_unstake(ctx: Context<Unstake>, mpsol_amount: u64) -> Result<()> {
+    // compute withdrawal fee
+    let withdrawal_fee_mpsol = {
+        if let Some(treasury_mpsol_account) = ctx.accounts.main_state.treasury_mpsol_account {
+            require_keys_eq!(
+                treasury_mpsol_account,
+                ctx.accounts.treasury_mpsol_account.key(),
+                ErrorCode::InvalidTreasuryMpsolAccount
+            );
+            apply_bp(mpsol_amount, ctx.accounts.main_state.withdraw_fee_bp)
+        } else {
+            0
+        }
+    };
+
     // compute the sol value of the mpsol to burn
     let ticket_sol_value = mpsol_amount_to_sol_value(
-        mpsol_amount,
+        mpsol_amount - withdrawal_fee_mpsol,
         ctx.accounts.main_state.backing_sol_value,
         ctx.accounts.mpsol_mint.supply,
     );
@@ -54,12 +70,28 @@ pub fn handle_unstake(ctx: Context<Unstake>, mpsol_amount: u64) -> Result<()> {
                 authority: ctx.accounts.unstaker.to_account_info(),
             },
         ),
-        mpsol_amount,
+        mpsol_amount - withdrawal_fee_mpsol,
     )?;
     // by removing from main_state.backing_sol_value,
     // mpSOL price remains the same after the burn
     ctx.accounts.main_state.backing_sol_value -= ticket_sol_value;
     // -------
+
+    // transfer withdrawal_fee_mpsol to treas
+    // Transfer tokens from vault to strat lst
+    if withdrawal_fee_mpsol > 0 {
+        anchor_spl::token::transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.unstaker_mpsol_account.to_account_info(),
+                    to: ctx.accounts.treasury_mpsol_account.to_account_info(),
+                    authority: ctx.accounts.unstaker.to_account_info(),
+                },
+            ),
+            withdrawal_fee_mpsol,
+        )?;
+    }
 
     // compute ticket due timestamp
     let now_ts = Clock::get().unwrap().unix_timestamp as u64;
@@ -89,7 +121,7 @@ pub fn handle_unstake(ctx: Context<Unstake>, mpsol_amount: u64) -> Result<()> {
         ticket_account: ctx.accounts.new_ticket_account.key(),
         ticket_sol_value,
         unstaker_mpsol_account: ctx.accounts.unstaker_mpsol_account.key(),
-        mpsol_burned: mpsol_amount,
+        mpsol_burned: mpsol_amount - withdrawal_fee_mpsol,
         ticket_due_timestamp,
         //--- mpSOL price components after the unstake
         main_vault_backing_sol_value: ctx.accounts.main_state.backing_sol_value,
