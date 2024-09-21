205749f216b16bcea4dfd2ab199e8e160075edae
Lucio Tato <luciotato@gmail.com>
2024-09-22 06:09:14 +0800

    fix-script: remove temp buffer signer

--- scripts/devnet/retry-deploy.sh
+++ scripts/devnet/retry-deploy.sh
@@ -1,13 +1,13 @@
 PROGRAM_LIB_NAME=mp_sol_restaking
 PROGRAM_KEYPAIR=~/.config/solana/MPSoLoEnfNRFReRZSVH2V8AffSmWSR4dVoBLFm1YpAW.json
-UPGRADE_AUTHORITY=~/.config/solana/MP5o14fjGUU6G562tivBsvUBohqFxiczbWGHrwXDEyQ.json
+UPGRADE_AUTHORITY=~/.config/solana/DEVYT7nSvD4gzP6BH2N1ubUamErS4TXtBYwdVrFBBVr.json
 if [ ! -f buffer-signer.json ]; then
     solana-keygen recover -o buffer-signer.json
 fi
 solana program deploy \
-    -u mainnet-beta \
+    -u d \
     target/deploy/$PROGRAM_LIB_NAME.so \
     --program-id $PROGRAM_KEYPAIR \
-    --upgrade-authority $UPGRADE_AUTHORITY \
+    -k $UPGRADE_AUTHORITY --upgrade-authority $UPGRADE_AUTHORITY \
     --buffer buffer-signer.json --max-sign-attempts 2 \
     && rm buffer-signer.json
