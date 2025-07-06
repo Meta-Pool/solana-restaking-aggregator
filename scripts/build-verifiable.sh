b9c2dc9ddce1377c73dcac0200851a8f382472c6
Lucio Tato <luciotato@gmail.com>
2025-07-06 11:43:44 +0200

    warn about program buffer size

--- scripts/build-verifiable.sh
+++ scripts/build-verifiable.sh
@@ -1,7 +1,7 @@
 # cargo install solana-verify
 set -ex
 PROGRAM_LIB_NAME=mp_sol_restaking
-echo anchor-build to get target/idl and target/types 
+echo anchor-build to get target/idl and target/types
 anchor build -p $PROGRAM_LIB_NAME
 mkdir -p res
 cp -u target/idl/mp_sol_restaking.json res
@@ -10,3 +10,9 @@ cp -u target/types/mp_sol_restaking.ts res
 echo solana-verify build to deploy
 solana-verify build --library-name $PROGRAM_LIB_NAME
 #solana-verify get-executable-hash target/deploy/$PROGRAM_LIB_NAME.so
+
+echo ------------------------------------------
+echo CHECK mainnet program SIZE BEFORE UPGRADE
+solana program show -u m MPSoLoEnfNRFReRZSVH2V8AffSmWSR4dVoBLFm1YpAW
+ls -l target/deploy/$PROGRAM_LIB_NAME.so
+echo ------------------------------------------
